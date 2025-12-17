168dc5bdfa33a53f1cf8e30763bb21fa2fdd1a2c
SiCheng Zhang <964389211@qq.com>
2025-12-17 17:18:48 +0800

    feat: cache shop-type

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_TTL;
 
 /**
  * <p>
@@ -16,5 +28,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryTypeList() {
+        // 从redis查询商铺类型缓存
+        String key = CACHE_SHOP_TYPE_KEY;
+        String shopTypeStr = stringRedisTemplate.opsForValue().get(key);
+        // 查询redis，若存在直接返回
+        if (StrUtil.isNotBlank(shopTypeStr)) {
+            List<ShopType> shopTypeList = JSONUtil.toList(shopTypeStr, ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+        // 查数据库
+        List<ShopType> shopTypeList = this.query().orderByAsc("sort").list();
+        if (shopTypeList == null || shopTypeList.isEmpty()) {
+            return Result.fail("未查询到此店铺类型");
+        }
+        // 写入redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shopTypeList), CACHE_SHOP_TYPE_TTL, TimeUnit.MINUTES);
+        return Result.ok(shopTypeList);
+    }
 }
